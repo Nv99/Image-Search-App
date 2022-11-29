cf653bb95685db492119d480d1e94c06b0454232
Naveen-1401 <naveen.verma@cogoport.com>
2022-11-29 12:44:46 +0530

    image-search

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+ 
+const searchImages = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos' , {
+        headers : {
+            Authorization: 'Client-ID SpVlnYH7qn_w6ly2NWy1-tc04Ge_H4bvzi9uAclBwDQ',
+        },
+        params : {
+            query : term,
+        },
+    });
+    
+    return response.data.results
+}; 
+
+export default searchImages;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import './SearchBar.css';
+
+function SearchBar({onSubmit}){
+
+    const [term , setTerm] = useState('');
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(term);
+    };
+
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+    }
+    
+    return (<div className="search-bar"> 
+                <form onSubmit={handleFormSubmit}>
+                    <label> Your Image Search Engine </label>
+                    <input value = {term} onChange = {handleChange} />
+                </form>
+           </div>
+    );
+
+}
+export default SearchBar;
